15e3b5b9b6d286ea2a094b2c72fbbceead122ffc
anshmodi <anshmodi16@gmail.com>
2021-04-07 15:42:20 +0530

    test commit

--- src/comps/ProgresBar.js
+++ src/comps/ProgresBar.js
@@ -4,6 +4,7 @@ import useStorage from '../Hooks/useStorage'
 export const ProgressBar = ({file, setFile})=>{
     const {url,progress} = useStorage(file)
     console.log(progress);
+    // test
     return(
         <div  style = {{width: progress + '%'}}>
             progress
